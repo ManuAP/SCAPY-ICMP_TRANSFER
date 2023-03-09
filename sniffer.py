1c39a98c9108757f3aaca7203b196405c1fe0f79
ManuAP <127395100+ManuAP@users.noreply.github.com>
2023-03-09 12:22:38 +0100

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -20,16 +20,3 @@ def dataParser(packet):
 if __name__ == '__main__':
 
         sniff(iface='tun0', prn=dataParser)
-
-EJ DEL OUTPUT:
-❯ python3 icmp_transfer.py
-
-127.0.0.1	localhost.localdomain	localhost
-::1		localhost6.localdomain6	localhost6
-
-# The following lines are desirable for IPv6 capable hosts
-::1     localhost ip6-localhost ip6-loopback
-fe00::0 ip6-localnet
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
-ff02::3 ip6-allhosts
